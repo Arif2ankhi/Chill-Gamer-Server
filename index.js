815b926a397fda1058889c96c25d6f7027691773
Arif2ankhi <arif2ankhi@gmail.com>
2024-12-05 04:52:50 +0000

    Added data from front end to mongoDB successfully

--- index.js
+++ index.js
@@ -15,15 +15,14 @@ app.use(express.json());
 // 5DaiOscr0eTVnqNf
 
 
-// console.log(process.env.DB_USER);
-// console.log(process.env.DB_PASS);
+
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsvzy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 console.log(uri);
-// const uri = "mongodb+srv://chillGamer:5DaiOscr0eTVnqNf@cluster0.nsvzy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -38,12 +37,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const gameCollection = client.db('gameDB').collection('game');
+
+    app.post('/game', async (req, res) => {
+      const newGame = req.body;
+      console.log(newGame);
+      const result = await gameCollection.insertOne(newGame);
+      res.send(result);
+
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
